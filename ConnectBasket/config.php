32fffba8d6514a8800d36cacbff0e3858c640547
Henry Fowler <fowlerh@flip3.engr.oregonstate.edu>
2018-02-10 17:39:17 -0800

    Restructured the code a little bit

---
+++ ConnectBasket/config.php
@@ -0,0 +1,7 @@
+<?php
+$details=array();
+$details['server_host']="localhost";//server host name
+$details['mysql_name']="root";//your mysql user name
+$details['mysql_password']="Healthydogsandcats";//your mysql user name
+$details['mysql_database']="ConnectBasket";//your database name
+?>
--- ConnectBasket/js/ConnectBasket.js
+++ ConnectBasket/js/ConnectBasket.js
@@ -1,25 +1,102 @@
 /**
-* Main AngularJS Web Application
-*/
-var app = angular.module('ConnectBasketWebApp', [
-'ngRoute'
-]);
+ * Main AngularJS Web Application
+ */
+/*var app = angular.module('ConnectBasketWebApp', [
+  'ngRoute'
+]);*/
 
 /**
+ * Configure the Routes
+ */
+ 
+ /**
 * Configure the Routes
 */
-app.config(function ($routeProvider) {
+/*app.config(function ($routeProvider) {
 $routeProvider
 // Home
-.when("/Home", {templateUrl: "modules/Home/Home.html"})
+.when("/Home", {templateUrl: "modules/Home.html", controller: "HomeController"})
 // Pages
-.when("/Login", {templateUrl: "modules/Login/Login.html"})
+.when("/Login", {templateUrl: "modules/Login.html", controller: "LoginController"})
 
-.when("/CreateUser", {templateUrl: "modules/CreateUser/CreateUser.html"})
+.when("/CreateUser", {templateUrl: "modules/CreateUser.html"})
 
-.when("/CreatePet", {templateUrl: "modules/CreatePet/CreatePet.html"})
+.when("/CreatePet", {templateUrl: "modules/CreatePet.html"})
 
-.when("/CreateOwner", {templateUrl: "modules/CreateOwner/CreateOwner.html"})
+.when("/CreateOwner", {templateUrl: "modules/CreateOwner.html"})
 // else 404
-.otherwise("/Home", {templateUrl: "modules/Home/Home.html"});
-});
+.otherwise("/Home", {templateUrl: "modules/Home.html"});
+});*/
+
+var app = angular.module('ConnectBasketWebApp', ['ui.router']);
+
+app.run(function($rootScope, $location, $state, LoginService) {
+    $rootScope.$on('$stateChangeStart', 
+      function(event, toState, toParams, fromState, fromParams){ 
+          console.log('Changed state to: ' + toState);
+      });
+    
+      if(!LoginService.isAuthenticated()) {
+        $state.transitionTo('login');
+      }
+  });
+  
+  app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/home');
+    
+    $stateProvider
+      .state('login', {
+        url : '/login',
+        templateUrl : 'modules/Login.html',
+        controller : 'LoginController'
+      })
+      .state('home', {
+        url : '/home',
+        templateUrl : 'modules/Home.html',
+        controller : 'HomeController'
+      })
+	  .state('createpet', {
+        url : '/createpet',
+        templateUrl : 'modules/CreatePet.html',
+        controller : 'CreatePetController'
+      });
+  }]);
+
+  app.controller('LoginController', function($scope, $rootScope, $stateParams, $state, LoginService) {
+    $rootScope.title = "LOGIN";
+		
+    $scope.formSubmit = function() {
+      if(LoginService.login($scope.username, $scope.password)) {
+        $scope.error = '';
+        $scope.username = '';
+        $scope.password = '';
+        $state.transitionTo('home');
+      } else {
+        $scope.error = "Incorrect username/password !";
+      }   
+    };
+	    
+  });
+  
+  app.controller('HomeController', function($scope, $rootScope, $stateParams, $state, LoginService) {
+    $rootScope.title = "HOME";
+    
+  });
+  
+  app.factory('LoginService', function() {
+    var admin = 'admin';
+    var pass = 'pass';
+    var isAuthenticated = false;
+    
+    return {
+      login : function(username, password) {
+        isAuthenticated = username === admin && password === pass;
+        return isAuthenticated;
+      },
+      isAuthenticated : function() {
+        return isAuthenticated;
+      }
+    };
+    
+  });
+  
