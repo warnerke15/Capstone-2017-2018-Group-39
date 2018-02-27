c8cfb6f56f3a814da812cefa88935b6f2c3224a9
Henry Fowler <fowlerh@flip1.engr.oregonstate.edu>
2018-02-26 20:46:04 -0800

    Fixed home buttons

--- ConnectBasket/wsdl.php
+++ ConnectBasket/wsdl.php
@@ -141,6 +141,90 @@ else if($data->method == "create_user")
  
 }
 
+else if($data->method == "create_owner")
+{
+	$firstname = $data->firstname;
+	$lastname = $data->lastname;
+	$username = $data->phonenumber;
+	$password = $data->email;
+	$email = $data->email;
+	$hashpass = password_hash($password, PASSWORD_DEFAULT);
+	$conn = new mysqli($details['server_host'], $details['mysql_name'],$details['mysql_password'], $details['mysql_database']);	
+	if ($conn->connect_error)
+	{
+		die("Connection failed: " . $conn->connect_error);
+	}
+	$stmt = $conn->prepare('Insert Into Users(Username, LastName, FirstName, Password, EmailAddress) Values(?,?,?,?,?)');
+	$stmt->bind_param('sssss', $username,$lastname,$firstname,$hashpass,$email); 
+
+	$stmt->execute();
+	
+	$stmt = $conn->prepare('SELECT Count(UsersTableID) FROM Users WHERE Username=? and Password=?');
+	$stmt->bind_param('ss', $username,$hashpass); 
+
+
+	$stmt->execute();
+
+	$result = $stmt->get_result();
+	if ($result->num_rows > 0)
+	{
+		$success = true;
+	}
+	else
+	{
+		$success = false;
+	}
+
+	$jsonData=array();
+	$jsonData['success']=$success;
+ 
+	$conn->close();
+	echo json_encode($jsonData);
+ 
+}
+
+else if($data->method == "create_pet")
+{
+	$firstname = $data->firstname;
+	$lastname = $data->lastname;
+	$username = $data->username;
+	$password = $data->password;
+	$email = $data->email;
+	$hashpass = password_hash($password, PASSWORD_DEFAULT);
+	$conn = new mysqli($details['server_host'], $details['mysql_name'],$details['mysql_password'], $details['mysql_database']);	
+	if ($conn->connect_error)
+	{
+		die("Connection failed: " . $conn->connect_error);
+	}
+	$stmt = $conn->prepare('Insert Into Users(Username, LastName, FirstName, Password, EmailAddress) Values(?,?,?,?,?)');
+	$stmt->bind_param('sssss', $username,$lastname,$firstname,$hashpass,$email); 
+
+	$stmt->execute();
+	
+	$stmt = $conn->prepare('SELECT Count(UsersTableID) FROM Users WHERE Username=? and Password=?');
+	$stmt->bind_param('ss', $username,$hashpass); 
+
+
+	$stmt->execute();
+
+	$result = $stmt->get_result();
+	if ($result->num_rows > 0)
+	{
+		$success = true;
+	}
+	else
+	{
+		$success = false;
+	}
+
+	$jsonData=array();
+	$jsonData['success']=$success;
+ 
+	$conn->close();
+	echo json_encode($jsonData);
+ 
+}
+
 else if($data->method == "create_message")
 {
 	$username = $data->username;
