f9750312481a64eea744eddd4b0e5046e0f20fc9
Henry Fowler <fowlerh@flip3.engr.oregonstate.edu>
2018-03-12 14:54:51 -0700

    Close prepared statement

--- ConnectBasket/wsdl.php
+++ ConnectBasket/wsdl.php
@@ -45,12 +45,16 @@ if($data->method == "check_login")
 		$success = false;
 	}
 	
+	$stmt->close();
+	
 	if ($success)
 	{
-		//$stmt = $conn->prepare('Insert Into LogMessages(LogMessage, Username, LogMessageTypesTableID) Values(?, ?, 1)');
-		//$stmt->bind_param('ss', 'logged in', $User); 
+		$stmt = $conn->prepare('Insert Into LogMessages(LogMessage, Username, LogMessageTypesTableID) Values(?, ?, 1)');
+		$stmt->bind_param('ss', 'logged in', $User); 
 
-		//$stmt->execute();
+		$stmt->execute();
+		
+		$stmt->close();
 	}
 
 	$jsonData=array();
