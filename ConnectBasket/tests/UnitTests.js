43b1271793efbda865d56f2e8791d066881aaede
Henry Fowler <fowlerh@flip3.engr.oregonstate.edu>
2018-03-13 10:25:14 -0700

    Changed unit test

--- ConnectBasket/tests/UnitTests.js
+++ ConnectBasket/tests/UnitTests.js
@@ -2,19 +2,56 @@ describe('Controllers', function(){ //describe your object type
 
     beforeEach(module('ConnectBasketWebApp')); //load module<br />
     describe('LoginController',function(){ //describe your app name<br />
-        var LoginController;
-		var scope
-        beforeEach(inject(function($controller, $rootScope){ //instantiate controller using $controller service
-            scope = $rootScope.$new();
-			LoginController = $controller('LoginController', {
-				scope : scope
-			});
-        }));
-        it('Mode should be fun', function(){  //write tests
+        
+        it('Should', function(){  //write tests
 			scope.username = 'Test';
 			scope.password = '12345678';
 			scope.formSubmit();
             expect(scope.error).toBe(''); //pass
         });
+		
+		it('should post data (object)', inject(function($http) {
+    
+		var $scope = {};
+
+		/* Code Under Test */
+		http.post("http://vm-cs462-g39.eecs.oregonstate.edu/wsdl.php", {
+			'method' : 'check_login',
+			'username' : 'Test',
+			'password' : '12345678'
+		})
+		.then(function (response) 
+		{
+			$scope.isAuthenticated = response.data.success; 
+			$scope.firstName = response.data.first;
+			$scope.lastName = response.data.last;
+			$scope.username = response.data.username;
+			$scope.email = response.data.email;
+		});
+		/* End Code */
+
+
+		$httpBackend
+		  .when('POST', 'http://localhost/auth', { 
+			'method' : 'check_login',
+			'username' : 'Test',
+			'password' : '12345678'
+		  })
+		  .respond({ 
+			success : true,
+			first : 'Test',
+			last : 'User',
+			username : 'Test',
+			email : 'Test@t.com'
+		  });
+
+
+		$httpBackend.flush();
+
+		expect($scope.isAuthenticated).toEqual(true);
+
+	  }));
+	  
+	  
     });
 });
